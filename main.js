aa75d7f6549083df2c7135c0842696dfd3e6bcd3
Anusha211209 <84378742+Anusha211209@users.noreply.github.com>
2022-06-06 18:38:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+noseX = 0;
+noseY = 0;
+leftWristX = 0;
+rightWristX = 0;
+difference = 0;
+
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 500);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log("PoseNet is initialized!");
+}
+
+function draw(){
+    canvas.background('grey');
+    fill('pink');
+    stroke('pink');
+    square(noseX, noseY, difference);
+    document.getElementById("square_side").innerHTML = "The size of the Square currently = " + difference;
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + "; noseY = " + noseY);
+        
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("Left Wrist X = " + leftWristX + "; Right Wrist X = " + rightWristX + "; Difference = " + difference);
+    }
+}
